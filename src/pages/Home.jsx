2190e9ec5818c3ff7789e251918fa78838ea586a
Jaiki Sharma <jack912062sharmas@gmail.com>
2026-01-12 00:39:12 +0530

    api hat gya

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -64,7 +64,7 @@ const Home = () => {
     try {
       const apiUrl = import.meta.env.VITE_API_URL;
 
-      const res = await fetch(`${apiUrl}/api`, {
+      const res = await fetch(`${apiUrl}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
